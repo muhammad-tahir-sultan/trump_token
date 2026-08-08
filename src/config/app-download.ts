d42f002e9edbe9c39b85c53e59df6ee201825a28
Mohammad Tahir <tahirsultanofficial@gmail.com>
2026-08-08 10:35:26 +0500

    ok kar..

--- src/config/app-download.ts
+++ src/config/app-download.ts
@@ -1,2 +1,2 @@
 export const APP_DOWNLOAD_URL =
-  "https://drive.usercontent.google.com/u/0/uc?id=1UicKNg6530A4E-SlE8e5PpHkl1lvkC-T&export=download";
+  "https://drive.usercontent.google.com/download?id=1G_iCi1dYuOEFV2OP4cwItv-3SSW1Rmlu&export=download&authuser=0";
